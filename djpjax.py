d178e713c1723f60eb92ef635607593a7d0a4fb3
noxan <noxan@redmoonstudios.de>
2012-08-30 13:00:38 +0200

    move helper functions from djpjax into pjax module init file

--- djpjax.py
+++ djpjax.py
@@ -2,6 +2,9 @@ import functools
 
 from django.views.generic.base import TemplateResponseMixin
 
+from pjax import _pjaxify_template_var, _pjaxify_template_name
+
+
 def pjax(pjax_template=None):
     def pjax_decorator(view):
         @functools.wraps(view)
@@ -50,18 +53,3 @@ class PJAXResponseMixin(TemplateResponseMixin):
                 names = _pjaxify_template_var(names)
         return names
 
-
-def _pjaxify_template_var(template_var):
-    if isinstance(template_var, (list, tuple)):
-        template_var = type(template_var)(_pjaxify_template_name(name) for name in template_var)
-    elif isinstance(template_var, basestring):
-        template_var = _pjaxify_template_name(template_var)
-    return template_var
-
-
-def _pjaxify_template_name(name):
-    if "." in name:
-        name = "%s-pjax.%s" % tuple(name.rsplit('.', 1))
-    else:
-        name += "-pjax"
-    return name
